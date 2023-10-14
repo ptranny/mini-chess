01114cbb61fdb27eb7ab8de572db1971aa5ead66
Phuong Tran <ptranny@gmail.com>
2023-10-14 16:19:56 +1300

    Change board generation code to size dynamically based on state

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import { Game } from './model.js'
 import { start } from './controller.js'
 
+const myGame = new Game([
+  [null, 'bk', 'bq', null],
+  [null, null, null, null],
+  [null, null, null, null],
+  [null, 'wq', 'wk', null],
+])
+
 // Make board tiles (only have to do this once)
 const board = document.getElementById('board')
-const boardSize = 4
 
-for (let row = 0; row < boardSize; row++) {
-  for (let col = 0; col < boardSize; col++) {
+for (let row = 0; row < myGame.board.length; row++) {
+  for (let col = 0; col < myGame.board.length; col++) {
     const tile = board.appendChild(document.createElement('div'))
     tile.classList.add('tile')
     tile.id = row.toString() + col.toString()
@@ -15,11 +21,4 @@ for (let row = 0; row < boardSize; row++) {
   }
 }
 
-start(
-  new Game([
-    [null, 'bk', 'bq', null],
-    [null, null, null, null],
-    [null, null, null, null],
-    [null, 'wq', 'wk', null],
-  ])
-)
+start(myGame)
